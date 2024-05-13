c8185ee914a49e8647543c3dcfe6a7089d9957d0
Jose Luis <sanchezmendozajoseluis9@gmail.com>
2024-05-13 00:34:23 -0600

    Add mailsender

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post, Request } from '@nestjs/common';
 import Stripe from 'stripe';
+import { WebhooksService } from './webhooks.service';
 
 @Controller('webhooks')
 export class WebhooksController {
+    constructor(private readonly webhookservice:WebhooksService){}
     private stripe = new Stripe(process.env.STRIPE_SECRET_API_KEY)
 
     @Post('payment-intent-success')
@@ -13,5 +15,10 @@ export class WebhooksController {
         const dataxd= await req.text();
         console.log("Data x2")
         console.log(dataxd);
+    }
+
+    @Post('sendEmail')
+    async sendEmail(@Body('message') message, @Body('name') name, @Body('sender') sender){
+        return this.webhookservice.sendEmail(message,name,sender);
     }
 }
--- src/webhooks/webhooks.service.ts
+++ src/webhooks/webhooks.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
 
 @Injectable()
-export class WebhooksService {}
+export class WebhooksService {
+
+    async sendEmail(message: string, name: string, sender: string) {
+        const key = process.env.PUBLIC_EMAIL_TOKEN;
+        console.log(key);
+        const mailerSend = new MailerSend({
+            apiKey: key
+        })
+        console.log("auth")
+        console.log(mailerSend);
+        const recipients = [new Recipient(`sanchezmendozajoseluis9@gmail.com`, `José Luis`)];
+        const sentFrom = new Sender("MS_2upbfz@trial-jpzkmgqy7pyl059v.mlsender.net", "portafolio");
+        const emailParams = new EmailParams()
+            .setFrom(sentFrom)
+            .setTo(recipients)
+            .setReplyTo(sentFrom)
+            .setSubject("Correo desde el portafolio")
+            .setHtml(`<div><p>Correo de ${name}: ${message}</p><p>Correo: ${sender}</p></div>`)
+            .setText("This is the text content");
+        console.log("Email params")
+        console.log(emailParams)
+        const send = await mailerSend.email.send(emailParams);
+        console.log("Enviado")
+        console.log(send);
+        return send;
+    }
+}
